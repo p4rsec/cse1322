cb69ab14d175f47c2bd925cdfa10bc15b81d8b53
Seth Beckman <beckmanseth@gmail.com>
2019-01-23 20:11:06 -0500

    Mostly done with Lab 1B

--- Lab - 1B/Binary.java
+++ Lab - 1B/Binary.java
@@ -1,15 +1,17 @@
+// Seth Beckman
+
 import java.util.*;
 import java.io.*;
 class Binary {
 	public static int BinarySearch(int[] arr, int target){
-		int low=0,high=arr.length,mid=0,iterations=0;
+		int low=0,high=arr.length,mid=0,index=0;
 		boolean found = false;
 		Arrays.sort(arr);
 		
 		while(found == false){
-			iterations++;
 			mid = (low+high)/2;
 			if(target == arr[mid]){
+				index = mid;
 				found = true;
 			}
 			else if(target<arr[mid]){
@@ -21,13 +23,26 @@ class Binary {
 			if(low>high-1 || high<low+1)
 				found = false;
 		}
-		iterations++;
 		if(found == true)
-			return iterations;
+			return index;
 		else
 			return -1;	
 	}
 
+	private static int[] randData(){
+			int[] data = new int[20];
+			int n = 0;
+			int min = -100; 
+			int max = 100;
+			long epoch = System.currentTimeMillis();
+			Random rand = new Random(epoch);
+			for(int i=0;i<20;i++){
+				n = rand.nextInt((max - min) + 1) + min;
+				data[i] = n;
+			}
+			
+			return data;
+		}
 	
 	public static void main(String[] args) {
 		
--- Lab - 1B/Linear.java
+++ Lab - 1B/Linear.java
@@ -1,23 +1,52 @@
+// Seth Beckman
+
 import java.util.*;
 import java.io.*;
 class Linear {
-	class LinearBinarySearch {
-		public static int LinearSearch(int[] arr, int target){
-			int iterations = 0;
-			for(int i = 0;i < arr.length;i++){
-				if(arr[i] == target){
-					iterations = i;
-				}
+	private static int[] randData(){
+			int[] data = new int[20];
+			int n = 0;
+			int min = -100; 
+			int max = 100;
+			long epoch = System.currentTimeMillis();
+			Random rand = new Random(epoch);
+
+			for(int i=0;i<20;i++){
+				n = rand.nextInt((max - min) + 1) + min;
+				data[i] = n;
 			}
-			iterations++;
-			if (iterations == 0){
-				iterations = -1;
-			}
-			return iterations;
+			
+			return data;
 		}
 
+	private static int LinearSearch(int[] arr, int target){
+		int index = -1;
+		for(int i = 0;i < arr.length;i++){
+			if(arr[i] == target){
+				index = i;
+				break;
+			}
+		}
+		return index;
+	}
 	
 	public static void main(String[] args) {
+		int[] nums1 = {1, 4, 4, 22, -5, 10, 21, -47, 23};
+		int[] nums2 = new int[20];
+		nums2 = randData();
 		
+		if(!(LinearSearch(nums1, 21) == -1)){
+			System.out.printf("The index for nums1 is %d.\n",LinearSearch(nums1,21));
+		}
+		else {
+			System.out.println("The index was not found.");
+		}
+		
+		if(!(LinearSearch(nums2, 85) == -1)){
+			System.out.printf("The index for nums1 is %d.\n",LinearSearch(nums2,85));
+		}
+		else {
+			System.out.println("The index was not found.");
+		}
 	}
 }
